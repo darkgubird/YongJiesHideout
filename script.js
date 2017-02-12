23858faa6ce03851e677813c2a8d79d555cee251
darkgubird <pyjavatar@gmail.com>
2017-02-13 00:03:45 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready(function() {
+
+	var $toggleButton = $('.toggle-button'),
+    	$menuWrap = $('.menu-wrap'),
+    	$sidebarArrow = $('.sidebar-menu-arrow');
+
+	// Hamburger button
+
+	$toggleButton.on('click', function() {
+		$(this).toggleClass('button-open');
+		$menuWrap.toggleClass('menu-show');
+	});
+
+});
